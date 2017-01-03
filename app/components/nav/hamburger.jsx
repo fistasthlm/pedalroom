3d49b3abb9a3150f4f75fb113a2a57e769a5cacf
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-01-03 11:16:10 +0100

    removed unused stuff

--- app/components/nav/hamburger.jsx
+++ app/components/nav/hamburger.jsx
@@ -1,9 +1,8 @@
 import React, { Component, PropTypes } from 'react';
-import { IndexLink } from 'react-router';
 
 export default class Hamburger extends Component {
    render(){
-      const {toggled, toggleMenu } = this.props;
+      const { toggleMenu } = this.props;
       return(
          <div className="hamburger-button">
             <button id="navbar-toggle-btn-left" className="toggle-button" onClick={toggleMenu.bind(this)}>

22afb80e854e6c65e3f77e6415a453952f898a88
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-01-06 18:39:24 +0100

    trying this instead..

--- app/server.js
+++ app/server.js
@@ -1,2 +1,15 @@
-var statik = require('statik');
-statik(process.env.PORT || 1337);
+
+const express = require('express');
+
+const port = process.env.PORT || 8080;
+const basedir = __dirname + '/public';
+
+const app = express();
+
+// HTML5 pushState: serve static files or index.html for everything else
+app.use(express.static(basedir));
+app.get('*', (req, res) => res.sendFile('index.html', { root: basedir }));
+
+// Start listening
+app.listen(port);
+console.log(`Server started at port ${port}`);

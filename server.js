c2aa90d25ead22df41eb2d5970e39e5cd9c90b7a
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-01-08 20:21:29 +0100

    removed requirejs

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require('express');
-const requirejs = require('requirejs');
 const path = require('path');
 
 const port = process.env.PORT || 4444;
 const basedir = __dirname + '/public';
-
-
-
-requirejs.config({
-   //Pass the top-level main.js/index.js require
-   //function to requirejs so that node modules
-   //are loaded relative to the top-level JS file.
-   nodeRequire: require
-});
-
-
 const app = express();
 
 app.use(express.static(basedir));

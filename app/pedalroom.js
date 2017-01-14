1606c2a11b081b583a63c5e9a1036d4040237871
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-01-14 13:40:45 +0100

    redone to hashhistory and links

--- app/pedalroom.js
+++ app/pedalroom.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import configureStore from './configure-store';
 import Root from 'containers/root';
 import Home from 'containers/home';
@@ -12,7 +12,7 @@ import Merch from 'containers/merch';
 document.addEventListener('DOMContentLoaded', () => {
    ReactDOM.render(
       <Provider store={configureStore()}>
-         <Router history={browserHistory}>
+         <Router history={hashHistory}>
             <Route path="/" component={Root}>
                <IndexRedirect to="/home"/>
                <Route path="home" component={Home} />

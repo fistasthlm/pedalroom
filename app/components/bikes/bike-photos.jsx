afc09900f5c512b7dbf36982557f9608e6175da7
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-02-05 00:40:48 +0100

    added lightbox feature for multiple images

--- app/components/bikes/bike-photos.jsx
+++ app/components/bikes/bike-photos.jsx
@@ -1,31 +1,89 @@
 import React, {PropTypes, Component } from 'react';
 import Image from '../../components/viewHelper/image';
+import Lightbox from 'react-image-lightbox';
 
 export default class BikePhotos extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         isOpen: false,
+         photoIndex: 0
+      };
+   }
+
    renderThumbnails(images) {
       return (
          <div className="image-thumbnails">
             {
                images.map((image, index) => {
-                  return <Image className="thumbnail"
-                                url={image[index].fields.file.url}
-                                resize={true}
-                                width={20}
-                                height={20} />;
+                  return(
+                     <div key={index}>
+                        <Image className="thumbnail"
+                               url={image.fields.file.url}
+                               resize={true}
+                               width="300"
+                               height="300" />
+                     </div>
+                  );
                })
             }
          </div>
       );
    }
 
+   toggleLightBox() {
+      this.setState({isOpen: !this.state.isOpen});
+   }
+
+   previousImage() {
+      const { images } = this.props;
+      const { photoIndex } = this.state;
+
+      this.setState({
+         photoIndex: (photoIndex + images.length - 1) % images.length,
+      });
+   }
+
+   nextImage() {
+      const { images } = this.props;
+      const { photoIndex } = this.state;
+
+      this.setState({
+         photoIndex: (photoIndex + 1) % images.length,
+      });
+   }
+
    render() {
       const { images } = this.props;
+      const { isOpen, photoIndex } = this.state;
       return(
          <div className="photos">
-            <Image className="big-picture" url={images[0].fields.file.url} width="800" height="800" resize={true} />
             {
-               images.length > 1 &&
-                  this.renderThumbnails(images)
+               images &&
+                  <div onClick={this.toggleLightBox.bind(this)}>
+                     <Image className="big-picture"
+                            url={images[0].fields.file.url}
+                            width="800"
+                            height="800"
+                            resize={true} />
+                     {
+                        images.length > 1 &&
+                           this.renderThumbnails(images)
+                     }
+                  </div>
+
+            }
+            {
+               isOpen &&
+                  <Lightbox mainSrc={images[photoIndex].fields.file.url}
+                            nextSrc={images[(photoIndex + 1) % images.length].fields.file.url}
+                            prevSrc={images[(photoIndex + images.length - 1) % images.length].fields.file.url}
+                            onMovePrevRequest={this.previousImage.bind(this)}
+                            onMoveNextRequest={this.nextImage.bind(this)}
+                            onCloseRequest={this.toggleLightBox.bind(this)}
+                            discourageDownloads={true}
+                            enableZoom={false} />
             }
          </div>
       );

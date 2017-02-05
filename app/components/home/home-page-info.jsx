ce840880a0fd41f5a870af092126739e62828e64
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-02-05 01:24:19 +0100

    Added component and style

---
+++ app/components/home/home-page-info.jsx
@@ -0,0 +1,11 @@
+import React, { PropTypes, Component } from 'react';
+
+export default class HomePageInfo extends Component {
+   render() {
+      return(
+         <div className="home">
+            Not a club, nor a gang. Just a group of drunks with a passion for bikes!
+         </div>
+      );
+   }
+}

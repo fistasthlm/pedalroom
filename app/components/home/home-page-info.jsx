e49914e614de53a9f7de3241f2097407b91e6017
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-02-05 23:42:38 +0100

    Added icons, probably need to set up some separate solution

--- app/components/home/home-page-info.jsx
+++ app/components/home/home-page-info.jsx
@@ -4,7 +4,11 @@ export default class HomePageInfo extends Component {
    render() {
       return(
          <div className="home">
-            Not a club, nor a gang. Just a group of drunks with a passion for bikes!
+            "Not a club, nor a gang. Just a group of drunks with a passion for bikes!"
+
+            <div className="instagram">
+               <i className="icon-instagram"></i>
+            </div>
          </div>
       );
    }

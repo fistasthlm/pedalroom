a7684e47bbec01b37519ef75430a65b8ba298d59
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-11-19 23:26:15 +0100

    intendation

--- app/components/bikes/bike-grid/bike-grid.jsx
+++ app/components/bikes/bike-grid/bike-grid.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { List } from 'immutable';
+import {List} from 'immutable';
 import BikeTile from '../bike-tile/bike-tile';
 
-export default function BikeGrid ({ bikes }) {
-   return(
-      <div className="bike-grid">
-         {
-            bikes.size > 0 &&
-               bikes.map(bike => {
-                  return <BikeTile key={bike.get('_id')}
-                                   bike={bike} />;
-               })
-         }
-      </div>
-   );
+export default function BikeGrid({bikes}) {
+    return (
+        <div className="bike-grid">
+            {
+                bikes.size > 0 &&
+                bikes.map(bike => {
+                    return (
+                        <BikeTile key={bike.get('_id')}
+                                     bike={bike} />
+                    );
+                })
+            }
+        </div>
+    );
 }
 
 BikeGrid.propTypes = {
-   bikes: PropTypes.instanceOf(List).isRequired,
+    bikes: PropTypes.instanceOf(List).isRequired,
 };

f543cf8934bff9332c236d79d52f4e0fd1e60d63
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-12-23 20:53:23 +0100

    bemified

--- app/components/bikes/bike-specs/bike-specs.jsx
+++ app/components/bikes/bike-specs/bike-specs.jsx
@@ -2,42 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Map } from 'immutable';
 
-export default function BikeSpecs({bike}) {
-   return (
-      <div className="bike-specs">
-         <ul className="property-group">
-            <li className="property-group-item">
-               Frame - {bike.get('frame')}
-            </li>
-            <li className="property-group-item">
-               Fork - {bike.get('fork')}
-            </li>
-            <li className="property-group-item">
-               Cranks - {bike.get('cranks')}
-            </li>
-            <li className="property-group-item">
-               Drivetrain - {bike.get('drivetrain')}
-            </li>
-            <li className="property-group-item">
-               Handlebars/Stem - {bike.get('handlebars')}
-            </li>
-            <li className="property-group-item">
-               Pedals - {bike.get('pedals')}
-            </li>
-            <li className="property-group-item">
-               Saddle/Seatpost - {bike.get('saddle')}
-            </li>
-            <li className="property-group-item">
-               Front wheel - {bike.get('frontWheel')}
-            </li>
-            <li className="property-group-item">
-               Rear wheel - {bike.get('rearWheel')}
-            </li>
-         </ul>
-      </div>
-   );
+export default function BikeSpecs({ bike }) {
+    console.log(bike);
+    return (
+        <div className="bike-specs">
+            <div className="bike-specs__property-group">
+                <div className="bike-specs__property-group-item">
+                    Frame - {bike.get('frame')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Fork - {bike.get('fork')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Cranks - {bike.get('cranks')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Drivetrain - {bike.get('drivetrain')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Handlebars/Stem - {bike.get('handlebars')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Pedals - {bike.get('pedals')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Saddle/Seatpost - {bike.get('saddle')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Front wheel - {bike.get('frontWheel')}
+                </div>
+                <div className="bike-specs__property-group-item">
+                    Rear wheel - {bike.get('rearWheel')}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 BikeSpecs.propTypes = {
-   bike: PropTypes.instanceOf(Map).isRequired
+    bike: PropTypes.instanceOf(Map).isRequired
 };
